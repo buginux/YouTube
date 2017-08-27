ca854cbf0e4ec45053449a79849dfec8a2e2e389
杨志超 <yzc1@meitu.com>
2017-08-27 18:10:32 +0800

    Add sliding menu

--- YouTube/Home/View/SettingMenu.swift
+++ YouTube/Home/View/SettingMenu.swift
@@ -10,22 +10,51 @@ import UIKit
 
 class SettingMenu: UIView {
     
+    lazy var menu: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = UIColor.white
+        
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        
+        menu.frame = CGRect(x: 0, y: frame.height, width: frame.width, height: 200)
+        addSubview(menu)
+    }
+    
+    func show() {
+        alpha = 0.0
+        menu.frame.origin.y = frame.height
+        UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseOut, animations: {
+            self.alpha = 1.0
+            self.menu.frame.origin.y = self.frame.height - 200
+        }, completion: nil)
+    }
+    
     static func show(inView view: UIView) {
         let menu = SettingMenu(frame: view.frame)
-        menu.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
         
         view.addSubview(menu)
-        
-        menu.alpha = 0.0
-        UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseOut, animations: {
-            menu.alpha = 1.0
-            
-        }, completion: nil)
+        menu.show()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         UIView.animate(withDuration: 0.33, animations: {
             self.alpha = 0.0
+            self.menu.frame.origin.y = self.frame.height + 200
         }, completion: { finished in
             self.removeFromSuperview()
         })
